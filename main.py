a73af0fc810d02a9732bb8b17762a76ca96766c2
Mathew Kalish <128283718+Wang-Junhao0220@users.noreply.github.com>
2025-03-03 16:44:13 +0800

    Update main.py
    add chatting in main

--- main.py
+++ main.py
@@ -7,19 +7,16 @@
 #
 # # Bug: 流式响应无法打印 2025-02-22-22：55：30
 # # 测试多轮对话功能
-# def test_multi_turn_conversation():
-#     # 第一次对话
-#     first_message = "Hello! How are you?"
-#     first_response = RequestResponse.request_response(first_message, "xdeepseekr1", stream_response=True)
-#     PrintContents.print_response(first_response, True, True)
-#
-#     # 第二次对话，传递历史消息
-#     second_message = "What did I say? tell me"
-#     second_response = RequestResponse.request_response(second_message, "xdeepseekr1", stream_response=True)
-#     PrintContents.print_response(second_response, True, True)
-#
-#
-# # 运行测试
+def test_multi_turn_conversation():
+    # 第一次对话
+    first_message = input("You:")
+    first_response = RequestResponse.request_response(first_message, "xdeepseekr1", stream_response=True)
+    PrintContents.print_response(first_response, True, True)
+    # 第二次对话，传递历史消息
+    #second_message = "What did I say? tell me"
+    #second_response = RequestResponse.request_response(second_message, "xdeepseekr1", stream_response=True)
+    #PrintContents.print_response(second_response, True, True)
+# 运行测试
 # test_multi_turn_conversation()
 
 # imports
@@ -34,6 +31,8 @@ from ui.logic_settingsWindow import LogicSettingsWindow
 
 
 def launch_settings_window():
+    while True:
+        test_multi_turn_conversation();
     os.environ['QT_ENABLE_HIGHDPI_SCALING'] = '1'
     through = QtCore.Qt.HighDpiScaleFactorRoundingPolicy.PassThrough
     QtWidgets.QApplication.setHighDpiScaleFactorRoundingPolicy(through)
